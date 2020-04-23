d8381069fdff25546dbcafe60512fa75fdc9ba63
ryannathanwilson <ryan.nathan.wilson@gmail.com>
2020-04-23 18:35:20 -0400

    operate the calculator with the keyboard

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+// Add a real divide symbol
+// Add a backspace icon
+// make arrow be a finger when you mousover
+// figure out the plus/minus
+// give feadback when buttons are pressed.
+
+
 const add = (a, b) => a + b;
 const subract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -36,7 +43,7 @@ const operators = [
 ];
 const clear = document.querySelector('#clear');
 const plusMinus = document.querySelector('#plusMinus');
-const percent = document.querySelector('#percent');
+const percent = document.querySelector('#backspace');
 const equal = document.querySelector('#equal');
 
 let op;
@@ -44,34 +51,84 @@ let firstValue;
 let secondValue;
 
 numbers.forEach(number => {
-    number.id.addEventListener('click', () => {
-        results.innerHTML = results.innerHTML + number.value;
-    });
+    number.id.addEventListener('click', numberFunction.bind(this,number.value));
 });
 
 operators.forEach(operator => {
-    operator.id.addEventListener('click', () => {
-        firstValue = results.innerHTML;
-        results.innerHTML = '';
-        op = operator.value;
-    })
+    operator.id.addEventListener('click', operatorFunction.bind(this,operator.value));
 })
-
-equal.addEventListener('click', () => {
-    secondValue = results.innerHTML;
-    console.log(firstValue);
-    console.log(secondValue);
-    console.log(op);
-    console.log(operate(firstValue,secondValue,op));    
-    results.innerHTML = operate(firstValue,secondValue,op);
-})
-
+equal.addEventListener('click', equalFunction);
 clear.addEventListener('click', () => {
     results.innerHTML = null;
     firstValue = null;
     secondValue = null;
     op = null;
 })
+function numberFunction(num) {
+    results.innerHTML = results.innerHTML + num;
+}
+function operatorFunction(operator) {
+    firstValue = results.innerHTML;
+        results.innerHTML = '';
+        op = operator;
+        console.log(op);
+        
+}
+function equalFunction() {
+    secondValue = results.innerHTML;
+    console.log(firstValue);
+    console.log(secondValue);
+    console.log(op);
+    console.log(operate(firstValue,secondValue,op));    
+    results.innerHTML = operate(firstValue,secondValue,op);
+}
+function backspaceFunction() {
+    results.innerHTML = results.innerHTML.substring(0, results.innerHTML.length - 1);
+}
+function clearFunction() {
+    results.innerHTML = null;
+}
+function plusMinusFunction() {
+    results.innerHTML = - results.innerHTML;
+}
+
+window.addEventListener('keydown', (e) => {
+    if (!isNaN(e.key) || e.key == '.') {
+        numberFunction(e.key);
+    }
+    switch(e.key){
+        case '=':
+            equalFunction();
+            break;
+        case 'Enter':
+            equalFunction();
+            break;
+        case 'Backspace':
+            backspaceFunction();
+            break;
+        case 'Delete':
+            clearFunction();
+            break;
+        case '/':
+            operatorFunction('division');
+            break;
+        case '*':
+            operatorFunction('multiplication');
+            break;
+        case '-':
+            operatorFunction('subtraction');
+        case '+':
+            operatorFunction('addition');
+    }
+    console.log(e);  
+});
+// window.addEventListener('keydown', (e) => {
+//     if (!isNaN(e.key) || e.key == '.') {
+//         numberFunction(e.key);
+//         console.log(e.key);  
+//     }
+// })
+// window.addEventListener('kedown')
 
 
 let symbol = 'division';
