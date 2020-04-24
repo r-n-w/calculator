ff72758bfb6eb76fc3fa954205a1f81d88a3675b
ryannathanwilson <ryan.nathan.wilson@gmail.com>
2020-04-24 19:16:09 -0400

    description added and better button feedback

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// make better order of operations
+
 const add = (a, b) => a + b;
 const subract = (a, b) => a - b;
 const multiply = (a, b) => a * b;
@@ -6,89 +8,117 @@ const divide = (a, b) => a / b;
 const operate = (a, b, operator) => {
     switch (operator) {
         case 'addition':
-            return add(a,b);
+            return add(a, b);
         case 'subtraction':
-            return subract(a,b);
+            return subract(a, b);
         case 'multiplication':
-            return multiply(a,b);
+            return multiply(a, b);
         case 'division':
-            return divide(a,b);
+            return divide(a, b);
     }
-}
+};
 const numbers = [
-    {id: document.querySelector('#one'), value: '1'},
-    {id: document.querySelector('#two'), value: '2'},
-    {id: document.querySelector('#three'), value: '3'},
-    {id: document.querySelector('#four'), value: '4'},
-    {id: document.querySelector('#five'), value: '5'},
-    {id: document.querySelector('#six'), value: '6'},
-    {id: document.querySelector('#seven'), value: '7'},
-    {id: document.querySelector('#eight'), value: '8'},
-    {id: document.querySelector('#nine'), value: '9'},
-    {id: document.querySelector('#zero'), value: '0'},
-    {id: document.querySelector('#decimal'), value: '.'}
+    { id: document.querySelector('#one'), value: '1' },
+    { id: document.querySelector('#two'), value: '2' },
+    { id: document.querySelector('#three'), value: '3' },
+    { id: document.querySelector('#four'), value: '4' },
+    { id: document.querySelector('#five'), value: '5' },
+    { id: document.querySelector('#six'), value: '6' },
+    { id: document.querySelector('#seven'), value: '7' },
+    { id: document.querySelector('#eight'), value: '8' },
+    { id: document.querySelector('#nine'), value: '9' },
+    { id: document.querySelector('#zero'), value: '0' },
+    { id: document.querySelector('#decimal'), value: '.' },
 ];
 const operators = [
-    {id: document.querySelector('#divide'), value: 'division'},
-    {id: document.querySelector('#multiply'), value: 'multiplication'},
-    {id: document.querySelector('#subtract'), value: 'subtraction'},
-    {id: document.querySelector('#add'), value: 'addition'},
+    { id: document.querySelector('#divide'), value: 'division' },
+    { id: document.querySelector('#multiply'), value: 'multiplication' },
+    { id: document.querySelector('#subtract'), value: 'subtraction' },
+    { id: document.querySelector('#add'), value: 'addition' }
 ];
+ 
 const clear = document.querySelector('#clear');
-const plusMinus = document.querySelector('#plusMinus');
-const percent = document.querySelector('#backspace');
+const backspace = document.querySelector('#backspace');
 const equal = document.querySelector('#equal');
+
 
 let op;
 let firstValue;
 let secondValue;
-
-numbers.forEach(number => {
-    number.id.addEventListener('click', numberFunction.bind(this,number.value));
-});
-operators.forEach(operator => {
-    operator.id.addEventListener('click', operatorFunction.bind(this,operator.value));
-})
-equal.addEventListener('click', equalFunction);
-clear.addEventListener('click', () => {
-    results.innerHTML = null;
-    firstValue = null;
-    secondValue = null;
-    op = null;
-})
+// Functions defining what happens when each button is clicked.
 function numberFunction(num) {
-    results.innerHTML = results.innerHTML + num;
+    if (results.innerHTML == 0) {
+        results.innerHTML = num;
+    } else {
+        results.innerHTML = results.innerHTML + num;
+    }
+    let activeButton = numbers.find((number) => number.value == num);
+    brightnessFlicker(activeButton.id);
 }
 function operatorFunction(operator) {
     firstValue = results.innerHTML;
-        results.innerHTML = '';
-        op = operator;
-        console.log(op);
-        
+    results.innerHTML = '0';
+    op = operator.value;
+    brightnessFlicker(operator.id);
 }
 function equalFunction() {
     secondValue = results.innerHTML;
-    console.log(firstValue);
-    console.log(secondValue);
-    console.log(op);
-    console.log(operate(firstValue,secondValue,op));    
-    results.innerHTML = operate(firstValue,secondValue,op);
+    results.innerHTML = operate(firstValue, secondValue, op);
+    brightnessFlicker(equal);
 }
 function backspaceFunction() {
-    results.innerHTML = results.innerHTML.substring(0, results.innerHTML.length - 1);
+    if (results.innerHTML.length === 1) {
+        results.innerHTML = 0;
+    } else {
+        results.innerHTML = results.innerHTML.substring(
+            0,
+            results.innerHTML.length - 1
+        );
+    }
+    brightnessFlicker(backspace);
 }
 function clearFunction() {
-    results.innerHTML = null;
+    results.innerHTML = 0;
+    firstValue = null;
+    secondValue = null;
+    op = null;
+    brightnessFlicker(clear);
 }
-function plusMinusFunction() {
-    results.innerHTML = - results.innerHTML;
+
+function brightnessFlicker(button) {
+    setTimeout(addHighlight.bind(this, button), 0);
+    setTimeout(removeHighlight.bind(this, button), 200);
 }
+function addHighlight(button) {
+    button.classList.add('highlight');
+}
+function removeHighlight(button) {
+    button.classList.remove('highlight');
+}
+
+//Assign Event listeners
+numbers.forEach((number) => {
+    number.id.addEventListener(
+        'click',
+        numberFunction.bind(this, number.value)
+    );
+});
+operators.forEach((operator) => {
+    operator.id.addEventListener(
+        'click',
+        operatorFunction.bind(this, operator)
+    );
+});
+equal.addEventListener('click', equalFunction);
+backspace.addEventListener('click', backspaceFunction)
+clear.addEventListener('click', clearFunction);
+
 
 window.addEventListener('keydown', (e) => {
     if (!isNaN(e.key) || e.key == '.') {
         numberFunction(e.key);
     }
-    switch(e.key){
+    switch (e.key) {
         case '=':
             equalFunction();
             break;
@@ -102,15 +132,14 @@ window.addEventListener('keydown', (e) => {
             clearFunction();
             break;
         case '/':
-            operatorFunction('division');
+            operatorFunction(operators[0]);
             break;
         case '*':
-            operatorFunction('multiplication');
+            operatorFunction(operators[1]);
             break;
         case '-':
-            operatorFunction('subtraction');
+            operatorFunction(operators[2]);
         case '+':
-            operatorFunction('addition');
+            operatorFunction(operators[3]);
     }
-    console.log(e);  
 });
